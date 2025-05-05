5e4f1d6a6b01ef132152c171a3fb22a54710e30e
AlexStry <150042828+AlexStry@users.noreply.github.com>
2025-05-04 23:01:44 -0600

    Create app.js

---
+++ Laboratorio14/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const app = express();
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+
+
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+
+const rutasFront = require('./routes/front');
+const rutasForm = require('./routes/form');
+const rutasJugador = require('./routes/jugador');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(session({
+  secret: 'secreto_super_seguro_123456789',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(express.static(path.join(__dirname, 'Sitio')));
+app.use('/', rutasFront);
+app.use('/form', rutasForm);
+app.use('/jugador', rutasJugador);
+
+app.use((req, res) => {
+  res.status(404).render('404', { titulo: 'Página no encontrada' }); 
+});
+
+app.listen(3000, () => {
+  console.log('Servidor iniciado en http://localhost:3000');
+});
